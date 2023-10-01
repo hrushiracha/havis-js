95c9392e73041ec70176ba8439774aeee34e0b96
Hrushikesh Rachawar <146446601+hrushiracha@users.noreply.github.com>
2023-10-01 19:01:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,333 @@
+var h1 = document.getElementsByTagName('h1');
+
+h1[0].style.fontFamily = "Centuma";
+
+var container = document.getElementsByClassName("container");
+
+container[0].style.width = "1170px";
+container[0].style.margin = "0 auto";
+container[0].style.paddingBottom = "99px";
+
+container[1].style.width = "1170px";
+container[1].style.margin = "0 auto";
+
+
+container[2].style.width = "1170px";
+container[2].style.margin = "0 auto";
+
+var main_content_left = document.getElementsByClassName("main_content_left");
+
+main_content_left[0].style.float = "left";
+main_content_left[0].style.width = "65%";
+
+var social = document.getElementsByClassName("social");
+
+social[0].style.padding = "20px 0";
+social[1].style.padding = "20px 0";
+social[2].style.padding = "20px 0";
+
+var span = document.getElementsByTagName("span");
+for (var i = 0; i < span.length; i++) {
+	span[i].style.color = "#95adb8";
+}
+var fa = document.getElementsByClassName("fa");
+for (var i = 0; i < fa.length; i++) {
+	fa[i].style.color = "#95adb8";
+}
+
+var p = document.getElementsByTagName("p");
+
+p[0].style.fontFamily = "Open Sans";
+p[0].style.padding = "50px 0";
+
+var a = document.getElementsByTagName("a");
+
+for (var i = 0; i < a.length; i++) {
+	a[i].style.color = "#0cc4ca";
+}
+
+// section two
+
+var h2 = document.getElementsByTagName('h2');
+
+h2[0].style.fontFamily = "Centuma";
+h2[0].style.fontSize = "26px";
+h2[0].style.padding = "50px 0 30px 0";
+
+var p = document.getElementsByTagName("p");
+
+p[1].style.fontFamily = "Open Sans Condensed";
+p[1].style.padding = "50px 0";
+p[1].style.fontSize = "20px";
+p[1].style.color = "#404b56";
+p[1].style.lineHeight = "26px";
+
+var main_content_left_second_inner = document.getElementsByClassName("main_content_left_second_inner");
+
+main_content_left_second_inner[0].style.padding = "40px 90px";
+main_content_left_second_inner[0].style.textAlign = "center";
+main_content_left_second_inner[0].style.border = "2px solid #e9eef1";
+main_content_left_second_inner[0].style.borderRadius = "2px";
+
+var span = document.getElementsByTagName('span');
+
+span[3].style.fontFamily = 'Open SansSb';
+span[3].style.marginTop = '30px';
+span[3].style.display = 'block';
+
+
+
+var h3 = document.getElementsByTagName('h3');
+
+h3[0].style.fontFamily = "Centuma";
+h3[0].style.fontSize = "24px";
+h3[0].style.padding = "50px 0 30px 0";
+
+p[2].style.color = "#404b56";
+p[2].style.fontSize = "16px";
+p[2].style.lineHeight = "26px";
+p[2].style.fontFamily = "Open Sans";
+
+span[4].style.fontFamily = "Open SansB"
+span[4].style.color = "#404b56";
+
+var h5 = document.getElementsByTagName("h5");
+
+
+h5[0].style.fontFamily = "Centuma";
+h5[0].style.fontSize = "20px";
+h5[0].style.padding = "50px 0 30px 0";
+h5[0].style.lineHeight = "30px";
+
+var aside = document.getElementsByTagName("aside");
+
+for (var i = 0; i < aside.length; i++) {
+	aside[i].style.float = "left";
+	aside[i].style.width = "50%";
+}
+
+aside[0].style.color = "#404b56";
+aside[0].style.fontSize = "16px";
+aside[0].style.lineHeight = "28px";
+aside[0].style.fontFamily = "Open Sans";
+
+
+aside[1].style.paddingLeft = "30px";
+
+var headingSix = document.getElementsByClassName("heading-six");
+
+for (var i = 0; i < headingSix.length; i++) {
+	headingSix[i].style.fontFamily = "Centuma";
+	headingSix[i].style.fontSize = "18px";
+	headingSix[i].style.lineHeight = "26px";
+	headingSix[i].style.padding = "50px 0 30px 0";
+}
+
+var flexC = document.getElementsByClassName("main_content_left_fifth");
+
+for (var i = 0; i < flexC.length; i++) {
+	flexC[i].style.display = "flex";
+}
+
+var main_content_left_fifth_content = document.getElementsByClassName("main_content_left_fifth_content");
+
+main_content_left_fifth_content[1].style.paddingLeft = "120px";
+
+listgroup = document.getElementsByClassName("listgroup");
+
+for (var i = 0; i < listgroup.length; i++) {
+	listgroup[i].style.fontFamily = "Open Sans";
+	listgroup[i].style.fontSize = "16px";
+	listgroup[i].style.lineHeight = "36px";
+	listgroup[i].style.color = "#404b56";
+}
+
+var ulgroup = document.getElementsByName('ulgroup');
+
+for (var i = 0; i < ulgroup.length; i++) {
+	ulgroup[i].style.listStylePosition = "inside";
+}
+
+var hfour = document.getElementsByClassName("hfour");
+
+hfour[0].style.fontFamily = "Centuma";
+hfour[0].style.fontSize = "20px";
+hfour[0].style.padding = "50px 0 30px 0";
+hfour[0].style.lineHeight = "30px";
+
+
+var sixthpara = document.getElementsByClassName("sixthpara");
+
+for (var i = 0; i < sixthpara.length; i++) {
+	sixthpara[i].style.fontFamily = "Open Sans";
+	sixthpara[i].style.fontSize = "16px";
+	sixthpara[i].style.lineHeight = "28px";
+	sixthpara[i].style.color = "#404b56";
+}
+
+var main_content_left_sixth_inner = document.getElementsByClassName("main_content_left_sixth_inner");
+
+for (var i = 0; i < main_content_left_sixth_inner.length; i++) {
+	main_content_left_sixth_inner[i].style.float = "left";
+	main_content_left_sixth_inner[i].style.width = "50%";
+}
+
+var seventhHeading = document.getElementsByClassName("seventhHeading");
+
+
+
+seventhHeading[0].style.fontFamily = "Centuma";
+seventhHeading[0].style.fontSize = "20px";
+seventhHeading[0].style.padding = "50px 0 30px 0";
+seventhHeading[0].style.lineHeight = "30px";
+
+var main_content_left_seventh_inner = document.getElementsByClassName("main_content_left_seventh_inner");
+
+for (var i = 0; i < main_content_left_seventh_inner.length; i++) {
+	main_content_left_seventh_inner[i].style.display = "flex";
+}
+
+var paraseventh = document.getElementsByClassName("paraseventh");
+
+for (var i = 0; i < paraseventh.length; i++) {
+	paraseventh[i].style.fontFamily = "Open Sans";
+	paraseventh[i].style.fontSize = "16px";
+	paraseventh[i].style.lineHeight = "28px";
+	paraseventh[i].style.color = "#0ea8b3";
+}
+
+var main_content_right = document.getElementsByClassName("main_content_right");
+
+main_content_right[0].style.float = "right";
+main_content_right[0].style.width = "30%";
+
+ var rightfh = document.getElementsByClassName("rightfh");
+ for (var i = 0; i < rightfh.length; i++) {
+ 	 rightfh[i].style.fontFamily = "Centuma";
+	 rightfh[i].style.fontSize = "18px";
+	 rightfh[i].style.padding = "30px 0 20px 0";
+	 rightfh[i].style.lineHeight = "18px";
+ }
+ 
+
+
+ var li = document.getElementsByTagName("li");
+
+ for (var i = 0; i < li.length; i++) {
+ 	li[i].style.listStyleType = "none";
+ }
+
+ var rightfa = document.getElementsByClassName("rightfa");
+
+ for (var i = 0; i < rightfa.length; i++) {
+ 	rightfa[i].style.color = "#0ea8b3";
+ 	rightfa[i].style.fontSize = "16px";
+ 	rightfa[i].style.lineHeight = "38px";
+ 	rightfa[i].style.fontFamily = "Open Sans";
+ 	rightfa[i].style.textDecoration = "none";
+ }
+
+var rightsp = document.getElementsByClassName("rightsp");
+
+	for (var i = 0; i < rightsp.length; i++) {
+		rightsp[i].style.color = "#0cc4ca";
+		rightsp[i].style.fontFamily = "Open Sans";
+		rightsp[i].style.fontSize = "16px";
+		rightsp[i].style.lineHeight = "28px";
+	}
+
+var righttp = document.getElementsByClassName("righttp");
+
+	righttp[0].style.color = "#7f878e";
+	righttp[0].style.fontSize = "14px";
+	righttp[0].style.fontFamily = "Open Sans";
+
+
+var rflink = document.getElementsByClassName("rflink");
+
+for (var i = 0; i < rflink.length; i++) {
+		rflink[i].style.textDecoration = "none";
+		rflink[i].style.color = "#34495e";
+		rflink[i].style.fontSize = "12pt";
+		rflink[i].style.lineHeight = "56px";
+		rflink[i].style.fontFamily = "Open Sans";
+		rflink[i].style.backgroundColor = "#e9eef1";
+		rflink[i].style.borderRadius = "5px";
+		rflink[i].style.padding = "10px 15px";
+	}	
+
+var flexContainer = document.getElementsByClassName("flex-container");
+
+for (var i = 0; i < flexContainer.length; i++) {
+		flexContainer[i].style.display = "flex";
+	}	
+
+	console.log(flexContainer);
+
+var prefooter = document.getElementsByClassName("prefooter");
+
+prefooter[0].style.backgroundColor = "#e9eef1";	
+prefooter[0].style.padding = "60px 0";	
+
+// var f-para = document.getElementsByClassName("f-para");
+
+// f-para[0].style.marginTop = "50px";
+
+var footer  = document.getElementsByTagName("footer");
+
+footer[0].style.backgroundColor = "#0d303e";
+footer[0].style.padding = "20px 0";
+
+var footer_content_left = document.getElementsByClassName("footer_content_left");
+
+footer_content_left[0].style.float = "left";
+
+footer_content_left[0].style.width = "20%";
+var footer_content_right = document.getElementsByClassName("footer_content_right");
+
+footer_content_right[0].style.float = "right";
+footer_content_right[0].style.width = "60%";
+
+
+var pfoot = document.getElementsByClassName("pfoot");
+pfoot[0].style.color = "#cad6dc";
+// pfoot[0].style.marginTop = "10px";
+
+var footeritem = document.getElementsByClassName("footeritem");
+
+for (var i = 0; i < footeritem.length; i++) {
+	footeritem[i].style.listStyleType = "none";
+	footeritem[i].style.display = "inline-block";
+	footeritem[i].style.marginRight = "30px";
+}
+
+var footerlink = document.getElementsByClassName("footerlink");
+
+for (var i = 0; i < footerlink.length; i++) {
+	footerlink[i].style.textDecoration = "none";
+	footerlink[i].style.color = "#cad6dc";
+}
+ 
+
+var pfheading = document.getElementsByClassName("pfheading");
+
+for (var i = 0; i < pfheading.length; i++) {
+	pfheading[i].style.marginBottom = "30px";
+	// pfheading[i].style.fontFamily = "Centuma";
+	pfheading[i].style.fontSize = "20px";
+	pfheading[i].style.color = "#404b56";
+
+
+
+
+}
+
+var finput = document.getElementsByClassName("finput");
+
+for (var i = 0; i < finput.length; i++) {
+	finput[i].style.marginBottom = "20px";
+	finput[i].style.width = "300px";
+	finput[i].style.height = "40px";
+	finput[i].style.borderRadius = "5px";
+	finput[i].style.border = "2px solid #c1cbd0";
+}
